d8716dff064e60dac57f0859d146d931b1e88549
Sergey Bogatyrev <bogatyrev.sergeyxxx@yandex.ru>
2025-07-10 15:52:55 +0500

    создал фикстуры и файл путей.

---
+++ yanews/pytest_tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+
+# Импортируем класс клиента.
+from django.test.client import Client
+
+# Импортируем модель заметки, чтобы создать экземпляр.
+from news.models import News
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+@pytest.fixture
+def author_client(author):
+    client = Client()
+    client.force_login(author)
+    return client
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)
+    return client
+
+@pytest.fixture
+def news(author):
+    News.objects.create(
+        title='Заголовок',
+        text='Текст',
+        date='Дата',
+        author=author
+    )
